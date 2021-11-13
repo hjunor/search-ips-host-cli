84865f7bcc4fe3a4f384b8948dd780945d18c889
Heberth Junor <hjunor@gmail.com>
2021-11-12 23:51:43 -0300

    search host in adders servers cli mod

--- app/app.go
+++ app/app.go
@@ -13,18 +13,27 @@ func Generator() *cli.App {
 
 	app.Name = "Plicação de linha de comando"
 	app.Usage = "Busca ips e nomes de servidores webs"
+
+	flags := []cli.Flag{
+		&cli.StringFlag{
+			Name:  "host",
+			Value: "google.com",
+			Usage: "Host a ser pesquisado",
+		},
+	}
+
 	app.Commands = []*cli.Command{
 		{
-			Name:  "ip",
-			Usage: "Busca de ips de indereços na internet",
-			Flags: []cli.Flag{
-				&cli.StringFlag{
-					Name:  "host",
-					Value: "google.com",
-					Usage: "Host a ser pesquisado",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca de ips de indereços na internet",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "server",
+			Usage:  "Busca de nomes de servidores na internet",
+			Flags:  flags,
+			Action: searchServer,
 		},
 	}
 	return app
@@ -39,6 +48,19 @@ func searchIps(c *cli.Context) error {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+	return nil
+}
+
+func searchServer(c *cli.Context) error {
+	host := c.String("host")
+	names, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, name := range names {
+		fmt.Println(name)
 	}
 	return nil
 }
